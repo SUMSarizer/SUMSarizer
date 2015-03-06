017e6fb6ad35fad5aa666f9f206f26b1f823f4c9
Marc Pare <marcpare0@gmail.com>
2015-03-05 22:03:29 -0800

    Upload many files, one by one

--- static/js/app.js
+++ static/js/app.js
@@ -7,20 +7,32 @@ $('#myform').on('submit', function (e) {
 	// request.open("POST", "/upload");
 	// request.send(new FormData(formElement));
 	
-	$('input[type="file"]').each(function () {
-		var file = this.files[0];
-		var formData = new FormData()
-		formData.append('file', file);
-		var request = new XMLHttpRequest();
-		request.open("POST", "/upload", true);
-		request.onload = function (e) {
-			console.log('uploaded');
-			console.log(e);
-			console.log(request.status);
-			console.log(JSON.parse(request.response));
+	var elInput = document.getElementById('files');
+	var files = Array.prototype.slice.call(elInput.files);
+	var uploadJobs = files.map(function (file, index) {
+		return function () {
+			var formData = new FormData()
+			formData.append('file', file);
+			var request = new XMLHttpRequest();
+			request.open("POST", "/upload", true);
+			request.onload = function (e) {
+				console.log('uploaded');
+				console.log(e);
+				console.log(request.status);
+				console.log(JSON.parse(request.response));
+				
+				nextJob();
+			}
+			request.send(formData);
 		}
-		request.send(formData);
-	})
+	});
+	
+	function nextJob () {
+		var job = uploadJobs.pop();
+		if (job) job();
+	}
+	
+	nextJob();
 	
 	
 	// var count = 0;
