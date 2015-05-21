07f03d3f547ec4b5345932cf274c50bbb42a51a5
Jeremy Coyle <jeremyrcoyle@gmail.com>
2015-05-21 01:33:03 -0700

    delete uploads even on errors. todo: add error feedback to user

--- worker.py
+++ worker.py
@@ -1,6 +1,5 @@
 import os
 
-import redis
 from StringIO import StringIO
 from app import app, db, zip_ingress
 from models import StudyUploads
@@ -10,8 +9,10 @@ while(1):
     study_uploads = StudyUploads.query.all()
     for study_upload in study_uploads:
         print "ingress for {0}".format(study_upload.filename)
-
-        zip_ingress(StringIO(study_upload.data), study_upload.study_id)
+        try:
+            zip_ingress(StringIO(study_upload.data), study_upload.study_id)
+        except:
+            pass
         db.session.delete(study_upload)
         db.session.commit()
     sleep(10)
