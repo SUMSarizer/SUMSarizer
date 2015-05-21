51b1b969156202b399c2289880970e3e31c51bc7
Jeremy Coyle <jeremyrcoyle@gmail.com>
2015-05-20 20:48:01 -0700

    rewrote upload to use bulk inserts, about a 3x speedup

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ import datetime
 import random
 import zipfile
 import re
-#from werkzeug.contrib.profiler import ProfilerMiddleware
+import sumsparser as parser
+from dateutil.parser import parse as date_parse
+
+from werkzeug.contrib.profiler import ProfilerMiddleware
 from flask import (
     Flask,
     redirect,
@@ -30,15 +33,17 @@ from stormpath.error import Error as StormpathError
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('APP_SETTINGS'))
-#app.config['PROFILE'] = True
-#app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+app.config['PROFILE'] = True
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
 db = SQLAlchemy(app)
 stormpath_manager = StormpathManager(app)
 
-from models import Datasets, Studies, DataPoints, Users
+from models import Datasets, Studies, DataPoints, Notes, Users
 
-SUBSET_SIZE=datetime.timedelta(7)
-##### Website
+SUBSET_SIZE = datetime.timedelta(7)
+# Website
+
+
 @app.route('/')
 def index():
     """Basic home page."""
@@ -68,9 +73,9 @@ def register():
     except StormpathError, err:
         # If something fails, we'll display a user-friendly error message.
         return render_template('register.html', error=err.message)
-    
+
     login_user(_user, remember=True)
-    
+
     # add user to internal DB
     db_user = Users(user)
     db.session.add(db_user)
@@ -110,9 +115,10 @@ def login():
 def dashboard():
     page = int(request.args.get('page') or 1)
     studies = Users.fromStormpath(user).studies
-    return render_template('dashboard.html', 
-      studies=studies)
-      
+    return render_template('dashboard.html',
+                           studies=studies)
+
+
 @app.route('/new_study', methods=['POST'])
 @login_required
 def new_study():
@@ -121,26 +127,28 @@ def new_study():
     study.users.append(Users.fromStormpath(user))
     db.session.commit()
     return redirect(url_for('dashboard'))
-    
+
+
 @app.route('/study/<id>', methods=['GET'])
 @login_required
 def study(id):
     study = Studies.query.get(id)
     if not study.authorizedUser(user):
         abort(401)
-    
+
     page = int(request.args.get('page') or 1)
-    
+
     datasets = study.datasets\
-      .order_by(Datasets.created_at.desc())\
-      .paginate(page, per_page=15)
-    
-    return render_template('study.html', 
-      study=study,
-      datasets=datasets.items,
-      pagination=datasets,
-      users=study.users.all())
-      
+        .order_by(Datasets.created_at.desc())\
+        .paginate(page, per_page=15)
+
+    return render_template('study.html',
+                           study=study,
+                           datasets=datasets.items,
+                           pagination=datasets,
+                           users=study.users.all())
+
+
 @app.route('/dataset/<id>', methods=['GET'])
 @login_required
 def dataset(id):
@@ -149,7 +157,7 @@ def dataset(id):
     dataset = Datasets.query.get(id)
     if not dataset.study.authorizedUser(user):
         abort(401)
-    
+
     # Grab the list of datasets in this study
     # What page is this dataset?
     # has_next?
@@ -157,7 +165,7 @@ def dataset(id):
     prev_ds = dataset.prev()
     next_ds = dataset.next()
     all_ds = dataset.items()
-    
+
     # studyname_ds = dataset.study_name()
 
     def clean_selected(sel):
@@ -167,10 +175,10 @@ def dataset(id):
             return 1
         return 0
 
-    data_points=dataset.data_points
-    if request.args.get('trainingOnly',''):
+    data_points = dataset.data_points
+    if request.args.get('trainingOnly', ''):
         data_points = data_points.filter_by(training=True).all()
-    
+
     graph_points = [{
         "id": x.id,
         "selected": 0,
@@ -179,84 +187,108 @@ def dataset(id):
         "selected": clean_selected(x.selected)
     } for x in data_points]
 
-    return render_template('dataset.html', 
-      dataset=dataset,
-      title=dataset.title,
-      study=dataset.study.title,
-      studyid = dataset.study_id,
-      notes=dataset.notes,
-      points=dataset.data_points,
-      points_json=json.dumps(graph_points),
-      next_ds=next_ds,
-      prev_ds=prev_ds,
-      all_ds=all_ds)
+    return render_template('dataset.html',
+                           dataset=dataset,
+                           title=dataset.title,
+                           study=dataset.study.title,
+                           studyid=dataset.study_id,
+                           notes=dataset.notes,
+                           points=dataset.data_points,
+                           points_json=json.dumps(graph_points),
+                           next_ds=next_ds,
+                           prev_ds=prev_ds,
+                           all_ds=all_ds)
+
 
 def point(json):
     point = DataPoints.query.get(json['id'])
     if not point.dataset.study.authorizedUser(user):
         abort(401)
-    
+
     point.selected = json['selected'] == 1
-    
+
     print point
     print point.timestamp
-    
+
     db.session.add(point)
     db.session.commit()
+
 
 @app.route('/points', methods=['POST'])
 @login_required
 def points():
     json = request.get_json()
-    
+
     for point_json in json:
         point(point_json)
-        
+
     return jsonify({
         "success": True
     })
-    
 
-def select_subset(dataset):
-    first_time=dataset.data_points[1].timestamp  
-    last_time=dataset.data_points[-1].timestamp
-    time_length=last_time-first_time-SUBSET_SIZE
-    subset_start=first_time+datetime.timedelta(seconds=random.random()* time_length.total_seconds())
-    subset_end=subset_start+SUBSET_SIZE
-    
-    for data_point in dataset.data_points:
-        if((subset_start<=data_point.timestamp)&(data_point.timestamp<=subset_end)):
-            data_point.training=True
-    
+
+def generate_selector(data):
+    first_time = date_parse(data[1][0])
+    last_time = date_parse(data[-1][0])
+    time_length = last_time-first_time-SUBSET_SIZE
+    subset_start = first_time + \
+        datetime.timedelta(
+            seconds=random.random() * time_length.total_seconds())
+    if subset_start < first_time:
+        subset_start = first_time
+    subset_end = subset_start+SUBSET_SIZE
+
+    def selector(timestamp):
+        return (subset_start <= timestamp) & (timestamp <= subset_end)
+
+    return selector
+
 
 @app.route('/upload/<id>', methods=['POST'])
 @login_required
 def upload(id):
+    conn = db.engine.connect()
     study = Studies.query.get(id)
     if study.owner != user.get_id():
         abort(401)
-    
+
     zfile = request.files['file']
-    zpf=zipfile.ZipFile(zfile)
-    valCSV=re.compile("[^\.\_.*\.csv]")
+    zpf = zipfile.ZipFile(zfile)
+    valCSV = re.compile("[^\.\_.*\.csv]")
     for file in zpf.namelist():
+        # make sure we have a real csv and not a folder or some other file type
         if(valCSV.match(file) is None):
             continue
         print file
-        dataset = Datasets.from_file(zpf.open(file),file, study)
-        select_subset(dataset)
+
+        # first add empty dataset to get key
+        dataset = Datasets(file, study)
         db.session.add(dataset)
+        db.session.commit()
+        dataset_id = dataset.id
+
+        # then parse file and add notes and points with Core bulk inserts
+        parsed = parser.parse(zpf.open(file))
+        selector = generate_selector(parsed['data'])
+
+        notes = [Notes.dict_from_parsed(note_text, dataset_id) for note_text in parsed['notes']]
+        data_points = [DataPoints.dict_from_parsed(parsed_point, dataset_id, selector) for parsed_point in parsed['data']]
+
+        # db.session.bulk_insert_mappings(DataPoints, data_points)
+        conn.execute(DataPoints.__table__.insert(), data_points)
+        # db.session.bulk_insert_mappings(Notes, notes)
+        conn.execute(Notes.__table__.insert(), notes)
         db.session.commit()
 
     return jsonify({
-    'success': True
+        'success': True
     })
 
 
 @app.route('/api', methods=['GET'])
 @login_required
 def api():
-  return jsonify({'hello': 'world'})
+    return jsonify({'hello': 'world'})
 
 
 @app.route('/logout')
@@ -268,4 +300,3 @@ def logout():
     """
     logout_user()
     return redirect(url_for('index'))
-
--- models.py
+++ models.py
@@ -1,133 +1,142 @@
 import datetime
 from app import db
-import sumsparser as parser
 from dateutil.parser import parse as date_parse
-    
+
 study_users = db.Table('study_users',
-    db.Column('study_id', db.Integer, db.ForeignKey('studies.id')),
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
-)
+                       db.Column('study_id', db.Integer, db.ForeignKey('studies.id')),
+                       db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
+                       )
 
 
 class Users(db.Model):
-  __tablename__ = 'users'
-  
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Unicode)
-  email = db.Column(db.Unicode)
-  studies = db.relationship('Studies', secondary=study_users,
-        backref=db.backref('users', lazy='dynamic'))
-  
-  def __init__(self, user):
-    self.user_id = user.get_id()
-    self.email = user.email
-    
-  @classmethod
-  def fromStormpath(cls,user):
-    print user.get_id()
-    return Users.query.filter_by(user_id=user.get_id()).first()
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Unicode)
+    email = db.Column(db.Unicode)
+    studies = db.relationship('Studies', secondary=study_users,
+                              backref=db.backref('users', lazy='dynamic'))
+
+    def __init__(self, user):
+        self.user_id = user.get_id()
+        self.email = user.email
+
+    @classmethod
+    def fromStormpath(cls, user):
+        print user.get_id()
+        return Users.query.filter_by(user_id=user.get_id()).first()
+
 
 class Studies(db.Model):
-  __tablename__ = 'studies'
-  
-  id = db.Column(db.Integer, primary_key=True)
-  created_at = db.Column(db.DateTime, default=datetime.datetime.now)
-  title = db.Column(db.Unicode)
-  owner = db.Column(db.Unicode)
-  
-  datasets = db.relationship('Datasets', lazy="dynamic", backref="study")
-  
-  #users = db.relationship('Users', secondary=study_users,
-  #      backref=db.backref('studies', lazy='dynamic'))
-  
-  def __init__(self, title, owner):
-    self.title = title
-    self.owner = owner.get_id()
-    
-  def authorizedUser(self,user):
-    return (self.users.filter_by(user_id=user.get_id()).count()>0)
+    __tablename__ = 'studies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    title = db.Column(db.Unicode)
+    owner = db.Column(db.Unicode)
+
+    datasets = db.relationship('Datasets', lazy="dynamic", backref="study")
+
+    # users = db.relationship('Users', secondary=study_users,
+    #      backref=db.backref('studies', lazy='dynamic'))
+
+    def __init__(self, title, owner):
+        self.title = title
+        self.owner = owner.get_id()
+
+    def authorizedUser(self, user):
+        return (self.users.filter_by(user_id=user.get_id()).count() > 0)
+
 
 class Datasets(db.Model):
-  __tablename__ = 'datasets'
+    __tablename__ = 'datasets'
 
-  id = db.Column(db.Integer, primary_key=True)
-  created_at = db.Column(db.DateTime, default=datetime.datetime.now)
-  title = db.Column(db.Unicode)
-  labelled = db.Column(db.Boolean)
-  
-  notes = db.relationship('Notes')
-  data_points = db.relationship('DataPoints', order_by="DataPoints.timestamp", backref="dataset",lazy="dynamic")
-  
-  study_id = db.Column(db.Integer, db.ForeignKey('studies.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    title = db.Column(db.Unicode)
+    labelled = db.Column(db.Boolean)
 
-  def __init__(self, title, study, notes = [], data_points = []):
-    self.title = title
-    self.study_id = study.id
-    self.notes = notes
-    self.data_points = data_points
-  
-  @classmethod
-  def from_file(cls, file, filename, study):
-    parsed = parser.parse(file)
-    
-    notes = [Notes(note_text) for note_text in parsed['notes']]
-    data_points=[DataPoints.from_parsed(parsed_point) for parsed_point in parsed['data']]
-    dataset = Datasets(filename, study, notes, data_points)
-    return dataset
-    
-  def next(self):
-    return Datasets.query\
-      .filter(Datasets.study_id==self.study_id)\
-      .filter(Datasets.created_at < self.created_at)\
-      .order_by(Datasets.created_at.desc())\
-      .first()
-        
-  def prev(self):
-    return Datasets.query\
-      .filter(Datasets.study_id==self.study_id)\
-      .filter(Datasets.created_at > self.created_at)\
-      .order_by(Datasets.created_at)\
-      .first()
+    notes = db.relationship('Notes')
+    data_points = db.relationship('DataPoints', order_by="DataPoints.timestamp", backref="dataset", lazy="dynamic")
 
-  def items(self):
-    return Datasets.query\
-      .filter(Datasets.study_id==self.study_id)\
-      .order_by(Datasets.created_at.desc())\
-      .all()
+    study_id = db.Column(db.Integer, db.ForeignKey('studies.id'))
+
+    def __init__(self, title, study, notes=[], data_points=[]):
+        self.title = title
+        self.study_id = study.id
+        self.notes = notes
+        self.data_points = data_points
+
+    def next(self):
+        return Datasets.query\
+            .filter(Datasets.study_id == self.study_id)\
+            .filter(Datasets.created_at < self.created_at)\
+            .order_by(Datasets.created_at.desc())\
+            .first()
+
+    def prev(self):
+        return Datasets.query\
+            .filter(Datasets.study_id == self.study_id)\
+            .filter(Datasets.created_at > self.created_at)\
+            .order_by(Datasets.created_at)\
+            .first()
+
+    def items(self):
+        return Datasets.query\
+            .filter(Datasets.study_id == self.study_id)\
+            .order_by(Datasets.created_at.desc())\
+            .all()
+
 
 class Notes(db.Model):
-  __tablename__ = 'notes'
-  
-  id = db.Column(db.Integer, primary_key=True)
-  created_at = db.Column(db.DateTime, default=datetime.datetime.now)
-  text = db.Column(db.Unicode)
-  
-  dataset_id = db.Column(db.Integer, db.ForeignKey('datasets.id'))
-  
-  def __init__(self, text):
-    self.text = text
+    __tablename__ = 'notes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    text = db.Column(db.Unicode)
+
+    dataset_id = db.Column(db.Integer, db.ForeignKey('datasets.id'))
+
+    def __init__(self, text):
+        self.text = text
+
+    @classmethod
+    def dict_from_parsed(cls, text, dataset_id):
+        return dict(
+            created_at=datetime.datetime.now(),
+            text=text,
+            dataset_id=dataset_id
+        )
+
 
 class DataPoints(db.Model):
-  __tablename__ = 'datapoints'
-  
-  id = db.Column(db.Integer, primary_key=True)
-  created_at = db.Column(db.DateTime, default=datetime.datetime.now)
-  timestamp = db.Column(db.DateTime)
-  unit = db.Column(db.String(16))
-  value = db.Column(db.Float)
-  selected = db.Column(db.Boolean)
-  # Add Boolean training set column
-  training = db.Column(db.Boolean)
-  
-  dataset_id = db.Column(db.Integer, db.ForeignKey('datasets.id'))
-  
-  def __init__(self, timestamp, unit, value):
-    self.timestamp = timestamp
-    self.unit = unit
-    self.value = value
-    
-  @classmethod
-  def from_parsed(cls, parsed_point):
-    return DataPoints(date_parse(parsed_point[0]),
-                              parsed_point[1],
-                              float(parsed_point[2]))
+    __tablename__ = 'datapoints'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    timestamp = db.Column(db.DateTime)
+    unit = db.Column(db.String(16))
+    value = db.Column(db.Float)
+    selected = db.Column(db.Boolean)
+    # Add Boolean training set column
+    training = db.Column(db.Boolean)
+
+    dataset_id = db.Column(db.Integer, db.ForeignKey('datasets.id'))
+
+    def __init__(self, timestamp, unit, value):
+        self.timestamp = timestamp
+        self.unit = unit
+        self.value = value
+
+    @classmethod
+    def dict_from_parsed(cls, parsed_point, dataset_id, selector):
+        timestamp = date_parse(parsed_point[0])
+        return dict(
+            created_at=datetime.datetime.now(),
+            timestamp=timestamp,
+            unit=parsed_point[1],
+            value=float(parsed_point[2]),
+            dataset_id=dataset_id,
+            selected=False,
+            training=selector(timestamp)
+        )
