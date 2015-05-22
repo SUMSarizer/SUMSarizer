d5625ea95a249ab9873e1ff826e1f92493280076
Ajay Pillarisetti <ajaypillarisetti@gmail.com>
2015-05-22 00:29:31 -0700

    Add JS filtering to list of users; tweak styles and layout

--- static/js/additional.js
+++ static/js/additional.js
@@ -14,5 +14,12 @@ $(function() {
 	    chart.attr("height", Math.round(targetWidth / aspect));
 	}).trigger("resize");
 
+    $('#searchDingus').keyup(function(){
+        var searchText = $(this).val();
+        $('ul#otherUsers > li').each(function(){
+            var currentLiText = $(this).text(),
+                showCurrentLi = currentLiText.indexOf(searchText) !== -1;            
+            $(this).toggle(showCurrentLi);
+        });     
+    });
 });
-
