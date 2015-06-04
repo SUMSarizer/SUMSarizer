e9249cae184c69cbc2822806d8fea72a8fb57744
Ajay Pillarisetti <ajaypillarisetti@gmail.com>
2015-06-04 11:15:53 -0700

    rename 401 error handler

--- app.py
+++ app.py
@@ -455,5 +455,9 @@ def logout():
     return redirect(url_for('index'))
 
 @app.errorhandler(401)
-def page_not_found(e):
+def unauthorized(e):
     return render_template('401.html'), 401
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
