5c0f21c94ad9e5550bd0a704a93c7cb45f3edf7a
Marc Pare <marcpare0@gmail.com>
2016-02-04 20:38:30 -0800

    Handle bad encodings more gracefully -- fixes the CSU bug

--- Vagrantfile
+++ Vagrantfile
@@ -16,7 +16,7 @@ Vagrant.configure("2") do |config|
   # Required for NFS
   config.vm.network :private_network, ip: "192.168.33.16"
 
-  config.vm.network :forwarded_port, guest: 5000, host: 5000
+  config.vm.network :forwarded_port, guest: 5005, host: 5005
   config.ssh.forward_agent = true
 
   config.vm.provider "virtualbox" do |v|
--- sumsparser.py
+++ sumsparser.py
@@ -4,10 +4,10 @@ def parse(file):
   NOTES = 1
   DATA = 2
   state = NOTES
-  
+
   notes = []
   data = []
-  
+
   for row in file:
     row = row.strip()
     if not row:
@@ -16,11 +16,17 @@ def parse(file):
       if re.match('^Date', row):
         state = DATA
       else:
+
+        # Strip non-ascii characters.
+        # This should really live in some sort of lib module, but it's only used here so far.
+        row = ''.join([i if ord(i) < 128 else ' ' for i in row])
+
+
         notes.append(row)
     elif state == DATA:
       data.append(row.split(','))
-    
+
   return {
     'notes': notes,
     'data': data
-  }   
+  }
