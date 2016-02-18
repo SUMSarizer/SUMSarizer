dd21753a59f81c65a33c81fa811d7d3eda559ca1
Marc Pare <marcpare0@gmail.com>
2016-02-17 22:48:41 -0800

    Fabfile for deploy and another tweak to test it

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,15 @@
+from fabric.api import *
 
+env.hosts = ['marc@50.116.4.99']
+
+def host_type():
+  run('uname -s')
+
+def deploy():
+  code_dir = '/home/jrcoyle/SUMSarizer'
+  with cd(code_dir):
+    run("git pull")
+    sudo("supervisorctl restart all")
 
 # Deploy
 
