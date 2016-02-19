c8488e989fbe076573561f5cc11847590a74a58c
Marc Pare <marcpare0@gmail.com>
2016-02-18 23:20:39 -0800

    Fabric task to take a db snapshot and download it

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,6 @@
+import os
+import datetime
+import time
 from fabric.api import *
 
 env.hosts = ['marc@50.116.4.99']
@@ -13,3 +16,12 @@ def deploy():
 
 # TODO: Migrations
   # run alembic on db
+
+def snapshot():
+  snapshot_dir = '/home/www'
+  outdir = 'dumps'
+  outfilename = "dump-%s" % datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d-%H%M%S')
+  outpath = os.path.join(outdir, outfilename)
+  with cd(snapshot_dir):
+    sudo('pg_dump -Fc --no-acl --no-owner sums > mydb.dump', user='www')
+  get('/home/www/mydb.dump', outpath)
