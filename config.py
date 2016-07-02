f29355ff0abe188c11aa89b82adf5a125c075f47
Marc Pare <marcpare0@gmail.com>
2016-07-02 16:13:36 -0700

    Prod config should get secret from environment

--- config.py
+++ config.py
@@ -33,6 +33,7 @@ class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'postgresql:///sums'
     ML_FOLDER = '/home/www/SUMSarizer/ml_labeler'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
